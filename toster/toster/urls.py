885d30dba06caf18e0981041830e90e65c0a30b1
Alexander Davydov <nyddle@gmail.com>
2014-07-09 22:02:59 +0400

    rest framework

--- toster/toster/urls.py
+++ toster/toster/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'toster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/', TemplateView.as_view()),
     url(r'^question/', QuestionView.as_view()),
