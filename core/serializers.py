4eb8dde832a9a85fe5e96eb595c2fb926d9b7871
Alexander Davydov <nyddle@gmail.com>
2014-07-16 00:15:41 +0400

    new structure

---
+++ core/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from core.models import Question, User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name',)
+        lookup_field = 'author'
+        view_name = 'user_detail'
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('question', 'author')
---
+++ core/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.http import Http404
+from django.http import HttpResponseNotFound
+from django.views.generic import View
+from django.views.generic import ListView
+from core.forms import AskQuestionForm
+from django.views.generic.edit import FormView, ProcessFormView, CreateView
+
+from core.models import Question, User
+
+from rest_framework import viewsets
+from core.serializers import QuestionSerializer, UserSerializer
+
+class QuestionView(View):
+    model = Question
+    def get(self, request, questionid):
+        try:
+            question = Question.objects.get(pk=questionid)
+            question.views += 1;
+            question.save()
+        except Question.DoesNotExist:
+            raise Http404
+        return render(request, 'core/question.html', {'question': question})
+
+class UserView(View):
+    model = User
+    def get(self, request, username):
+        try:
+            user = User.objects.get(name=username)
+        except User.DoesNotExist:
+            raise Http404
+        return render(request, 'core/user.html', {'user': user})
+
+
+#TODO: merge with popular view
+class QuestionListView(ListView):
+    model = Question
+    queryset = Question.objects.order_by('-pub_date')
+
+class PopularQuestionListView(ListView):
+    model = Question
+    queryset = Question.objects.order_by('-rating')
+
+class UserListView(ListView):
+    model = User
+
+class UserQuestionListView(ListView):
+    model = Question
+
+    def get_queryset(self):
+        self.author = get_object_or_404(User, name=self.kwargs['username'])
+        return Question.objects.filter(author=self.author)
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'base.html')
+
+
+class AskQuestionView(FormView):
+    template_name = 'core/new_question.html'
+    form_class = AskQuestionForm
+    success_url = '/questions'
+    def form_valid(self, form):
+        form.save()
+        return super(AskQuestionView, self).form_valid(form)
