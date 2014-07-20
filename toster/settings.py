9673e4bd9097fc2eecd5fab257945e1617a6f00d
Andrey Rusanov <rusanov.andrey.mail@gmail.com>
2014-07-20 14:49:24 +0300

    Refactor urks

--- toster/settings.py
+++ toster/settings.py
@@ -114,6 +114,35 @@ SOUTH_MIGRATION_MODULES = {
     'taggit': 'taggit.south_migrations',
 }
 
+# social auth
+AUTHENTICATION_BACKENDS = (
+    'social_auth.backends.twitter.TwitterBackend',
+    'social_auth.backends.facebook.FacebookBackend',
+    'social_auth.backends.google.GoogleOAuthBackend',
+    'social_auth.backends.google.GoogleOAuth2Backend',
+    'social_auth.backends.google.GoogleBackend',
+    'social_auth.backends.yahoo.YahooBackend',
+    'social_auth.backends.browserid.BrowserIDBackend',
+    'social_auth.backends.contrib.linkedin.LinkedinBackend',
+    'social_auth.backends.contrib.disqus.DisqusBackend',
+    'social_auth.backends.contrib.livejournal.LiveJournalBackend',
+    'social_auth.backends.contrib.orkut.OrkutBackend',
+    'social_auth.backends.contrib.foursquare.FoursquareBackend',
+    'social_auth.backends.contrib.github.GithubBackend',
+    'social_auth.backends.contrib.vk.VKOAuth2Backend',
+    'social_auth.backends.contrib.live.LiveBackend',
+    'social_auth.backends.contrib.skyrock.SkyrockBackend',
+    'social_auth.backends.contrib.yahoo.YahooOAuthBackend',
+    'social_auth.backends.contrib.readability.ReadabilityBackend',
+    'social_auth.backends.contrib.fedora.FedoraBackend',
+    'social_auth.backends.OpenIDBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGIN_URL = '/login-form/'
+LOGIN_REDIRECT_URL = '/logged-in/'
+LOGIN_ERROR_URL = '/login-error/'
+
 try:
     from .local_settings import *
 except:
--- toster/urls.py
+++ toster/urls.py
@@ -4,9 +4,6 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
-from core.views import HomeView, QuestionView, UserView, QuestionListView, PopularQuestionListView, \
-                        UserListView, AskQuestionView, UserView, UserQuestionListView, TagListView
-
 from rest_framework import routers
 from core import views
 
@@ -15,30 +12,14 @@ router.register(r'/api/users', views.UserViewSet)
 router.register(r'/api/questions', views.QuestionViewSet)
 
 urlpatterns = patterns('',
-
-    url(r'^$', QuestionListView.as_view(), name='home'),
-    url(r'^question/ask', AskQuestionView.as_view(), name='ask_question'),
-    url(r'^question/(?P<questionid>.+)', QuestionView.as_view(), name='question'),
-
-    url(r'^questions/latest/$', QuestionListView.as_view(), name='questions_latest'),
-    url(r'^questions/popular/$', PopularQuestionListView.as_view(), name='questions_popular'),
-    url(r'^questions/$', QuestionListView.as_view(), name='questions'),
-    url(r'^user/(?P<username>.+)/questions', UserQuestionListView.as_view(), name='user_questions'),
-
-    url(r'^user/(?P<username>.+)', UserView.as_view(), name='user'),
-    url(r'^users/$', UserListView.as_view(), name='users'),
-
-    url(r'^home/$', AskQuestionView.as_view(), name='home'),
+    url(r'^', include('core.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/', TemplateView.as_view(template_name='about.html'), name='about'),
-
-    url(r'^search2/$', QuestionListView.as_view(), name='search_results'),
     url(r'^search/', include('haystack.urls')),
-
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    url(r'^tags/', TagListView.as_view(template_name='core/tag_list.html'), name='tags'),
-    url(r'^tag/(?P<tag>.+)', QuestionListView.as_view(template_name='question_list.html'), name='tag'),
+    # social auth
+    url(r'', include('social_auth.urls')),
 
 )
 
