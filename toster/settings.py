8285496f535f9c79c7617d30a8e5f93d6be55206
Alexander Davydov <nyddle@gmail.com>
2014-07-25 00:29:45 +0400

    debug toolbar

--- toster/settings.py
+++ toster/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'debug_toolbar',
     'taggit',
     'bookmarks',
     'south',
