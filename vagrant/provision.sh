339bf44f55a509364ef5edc4b582fec353c2e639
Alexander Davydov <nyddle@gmail.com>
2014-08-15 01:06:21 +0400

    add elasticsearch

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,6 +1,25 @@
 #! /bin/sh
 
-sudo apt-get install python3
-sudo apt-get install python-pip
-sudo apt-get install python3-dev
-sudo apt-get install libpq-dev postgresql
+sudo apt-get install -y python3
+sudo apt-get install -y python-pip
+sudo apt-get install -y python3-dev
+sudo apt-get install -y libpq-dev postgresql
+
+
+## INSTALL ELASTICSEARCH
+
+# update apt
+sudo apt-get update
+# install java
+sudo apt-get install openjdk-7-jre-headless -y
+# install elasticsearch
+
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.1.deb
+dpkg -i elasticsearch-1.3.1.deb
+
+
+sudo service elasticsearch start
+
+
+sudo apt-get install -y git
+git clone https://github.com/nyddle/toster.git
